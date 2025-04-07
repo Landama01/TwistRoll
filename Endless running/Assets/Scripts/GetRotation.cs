845f4fc38edb3a31ca7783ac3d9a61edb1820fb9
Andreu Landa <andreulanda01@gmail.com>
2025-04-07 15:59:20 +0200

    Collectable system v3 (mimic ref position)

--- Endless running/Assets/Scripts/GetRotation.cs
+++ Endless running/Assets/Scripts/GetRotation.cs
@@ -4,39 +4,17 @@ using UnityEngine;
 
 public class GetRotation : MonoBehaviour
 {
-    public bool Collectable = false;
-
     public GameObject Click_Drag;
-    private float RecivedRotationX;
-    
-    int randomRotation = 0;
 
     void Start()
     {
         Click_Drag = GameObject.Find("ClickDrag");
 
         transform.rotation = Click_Drag.transform.rotation;
-
-        if(Collectable){
-            randomRotation = Random.Range(0, 10) * 36;
-        }
     }
 
     private void Update()
-    {
-        if (!Collectable)
-        {
-            transform.rotation = Click_Drag.transform.rotation;
-
-        }
-        else if (Collectable)
-        {
-            CollectableRotation(Click_Drag);
-        }
-    }
-
-    void CollectableRotation(GameObject ClickDrag)
     {        
-        transform.rotation = Quaternion.Euler(ClickDrag.transform.rotation.eulerAngles.x + randomRotation, 90, 90);
+       transform.rotation = Click_Drag.transform.rotation;        
     }
 }
